5458377489af3f7ec18cc3662eb3c72fe4e59a30
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-16 14:48:42 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * using mysqli_connect for database connection
  */
 
-$databaseHost = 'localhost'; # database server name
+$databaseHost = '172.31.40.223'; # database server name
 $databaseName = 'crud_db'; # database name
 $databaseUsername = 'root'; # database username
 $databasePassword = 'Anjan#123'; # database password
