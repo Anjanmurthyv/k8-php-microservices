a7be1845ffa18d3670ea4d9f870e942a34b47e79
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-16 14:47:22 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * using mysqli_connect for database connection
+ */
+
+$databaseHost = 'localhost'; # database server name
+$databaseName = 'crud_db'; # database name
+$databaseUsername = 'root'; # database username
+$databasePassword = 'Anjan#123'; # database password
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
