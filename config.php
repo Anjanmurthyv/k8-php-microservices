af161d89c783bde0af739b2134114a55e2fe8a2c
Anjanmurthyv <154734125+Anjanmurthyv@users.noreply.github.com>
2024-01-20 21:22:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@
 $databaseHost = '172.31.40.223'; # database server name
 $databaseName = 'crud_db'; # database name
 $databaseUsername = 'root'; # database username
-$databasePassword = 'Anjan#123'; # database password
+$databasePassword = 'King#123'; # database password
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
